fc08b992718b2b0ba0a66ed1c9501537bcdcaf86
Sreerag-ibtl <sreeragibtl@gmail.com>
2022-10-29 21:26:19 +0530

    dev: Add InputConvolutionLayer

--- architectures/densenet_121_input_convolution_layer.py
+++ architectures/densenet_121_input_convolution_layer.py
@@ -5,3 +5,14 @@ Construct densenet_121_input_convolution_layer.
 Usage:
     Implicit usage.
 """
+
+
+class InputConvolutionLayer:
+    """Input convolution layer.
+
+    The input convolution layer.
+    """
+
+
+if __name__ == "__main__":
+    InputConvolutionLayer()
