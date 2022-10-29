d7baa920eaea4c3d0325dadd2f35304a932c6294
Sreerag-ibtl <sreeragibtl@gmail.com>
2022-10-29 21:07:05 +0530

    dev: Test InputConvolutionLayer exist

---
+++ architectures/__init__.py
@@ -0,0 +1,9 @@
+"""Construct architectures.
+
+Construct package architectures.
+
+Usage:
+    Implicit usage.
+"""
+
+import densenet_121
---
+++ tests/test_input_convolution.py
@@ -0,0 +1,26 @@
+"""Test input convolution layer.
+
+Test the input convolution layer.
+
+Usage:
+    python -m pytest Architectures/test_input_convolution
+"""
+
+import architectures
+
+
+def test_input_convolution_exist() -> None:
+    """Test the input convolution exist.
+
+    Test the input convolution layer exist.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    """
+    assert hasattr(
+        architectures.densenet_121.input_convolution_layer, "InputConvolutionLayer"
+    )
