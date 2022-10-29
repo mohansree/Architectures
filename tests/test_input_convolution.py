f164bf11fde0bc02584a2afc990df93e3bb0592c
Sreerag-ibtl <sreeragibtl@gmail.com>
2022-10-29 21:31:51 +0530

    test: Assert InputConvolutionLayer subclasses torch module

--- tests/test_input_convolution.py
+++ tests/test_input_convolution.py
@@ -6,6 +6,8 @@ Usage:
     python -m pytest Architectures/test_input_convolution
 """
 
+import torch
+
 from architectures import densenet_121_input_convolution_layer
 
 
@@ -22,3 +24,20 @@ def test_input_convolution_exist() -> None:
 
     """
     assert hasattr(densenet_121_input_convolution_layer, "InputConvolutionLayer")
+
+
+def test_input_convolution_subclass_nn_module() -> None:
+    """Assert InputConvolutionLayer is torch module.
+
+    Assert InputConvolutionLayer is a torch module.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    """
+    assert issubclass(
+        densenet_121_input_convolution_layer.InputConvolutionLayer, torch.nn.Module
+    )
