ede12094877634ccdce3d132c7492b9cd694c50d
Sreerag-ibtl <sreeragibtl@gmail.com>
2022-11-05 19:15:53 +0530

    dev: relu_2 as ReLU

--- architectures/dense_121_dense_block.py
+++ architectures/dense_121_dense_block.py
@@ -47,5 +47,5 @@ class DenseLayer(torch.nn.Module):  # dead: disable
         self.batch_norm_2: None = torch.nn.BatchNorm2d(
             1,
         )
-        self.relu_2: None = None
+        self.relu_2: None = torch.nn.ReLU()
         self.conv_2: None = None
